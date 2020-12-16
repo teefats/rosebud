dd7c616da94e49ba454c38f91f23f10326dad900
teefats <tee_fats@yahoo.com>
2020-12-16 14:45:26 +0000

    Added form tests

--- boards/tests.py
+++ boards/tests.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from .views import home, board_topics, new_topic
 from .models import Board, Topic, Post
+from .forms import NewTopicForm
 
 
 
@@ -118,6 +119,18 @@ class NewTopicTests(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertFalse(Topic.objects.exists())
         self.assertFalse(Post.objects.exists())
+    
+    def test_contains_form(self):
+        url = reverse('new_topic', kwargs={'pk':1})
+        response = self.client.get(url)
+        form = response.context.get('form')
+        self.assertIsInstance(form, NewTopicForm)
+    def test_new_topic_invalid_post_date(self):
+        url = reverse('new_topic', kwargs={'pk':1})
+        response = self.client.post(url, {})
+        form = response.context.get('form')
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(form.errors)
 
 
     
