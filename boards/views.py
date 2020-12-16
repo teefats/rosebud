f9b2a45223a55fafe6c06a66d2be43cb5fa0f108
teefats <tee_fats@yahoo.com>
2020-12-16 12:31:17 +0000

    Added new post field

--- boards/views.py
+++ boards/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.models import User
 from django.db.models.query import RawQuerySet
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
-from .models import Board, Topic
+from .models import Board, Topic, Post
 
 # Create your views here.
 
@@ -27,6 +28,25 @@ def trial(request):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
+    
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message =request.POST['message']
+
+        user = User.objects.first()
+
+        topic = Topic.objects.create(
+            subject = subject,
+            board = board,
+            starter = user
+        )
+
+        post = Post.objects.create(
+            message = message,
+            topic = topic,
+            created_by=user
+        )
+        return redirect('board_topics', pk=board.pk)
     return render(request, 'new_topic.html', {'board':board})
 
 
